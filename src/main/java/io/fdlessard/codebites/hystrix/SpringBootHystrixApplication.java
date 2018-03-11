2ab72eb41e82bc484e65ebb8c36746cd6ca25645
fdlessard <fdlessard@gmail.com>
2018-03-11 15:24:01 -0400

    first commit

---
+++ src/main/java/io/fdlessard/codebites/hystrix/SpringBootHystrixApplication.java
@@ -0,0 +1,12 @@
+package io.fdlessard.codebites.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHystrixApplication.class, args);
+	}
+}
